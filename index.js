43e5feebdbe5f9f4428837e6481d566ed9a1ffc2
durunsong <durunsong@sailvan.com>
2024-09-26 16:12:55 +0800

    🌐chore:跨域配置

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express()
 const PORT = 4000
 app.use(
     cors({
-      origin: ["https://kilyicms-server.vercel.app", "http://localhost:7000", "http://localhost:4000"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
+        origin: ["https://kilyicms-server.vercel.app", "https://vercel.app", "https://vercel.com", "http://localhost:7000", "http://localhost:4000"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
     })
-  );
+);
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
@@ -21,18 +21,18 @@ app.use('/', homeRouter)
 app.use('/api/users/login', loginRouter)
 app.use('/echo', echoRouter)
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404))
 })
-  
-app.use(function(err, req, res, next) {
-res.locals.message = err.message
-res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-res.status(err.status || 500)
-res.render('error')
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    res.status(err.status || 500)
+    res.render('error')
 })
-  
+
 app.listen(PORT, () => console.log("Listening at ", PORT))
 
 module.exports = app 
