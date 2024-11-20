94136036249e1b16e4e19aa164d6e6afd793a8a7
durunsong <durunsong@sailvan.com>
2024-11-20 09:58:15 +0800

    🍊fix：修改

--- routes/login.js
+++ routes/login.js
@@ -33,6 +33,9 @@ router.post('/', async (req, res) => {
         res.cookie("kilyicms_token", token, {
             maxAge: 2 * 60 * 1000,
             httpOnly: true,
+            signed: false, 
+            secure: true, // 如果是 https 环境下
+            sameSite: "Lax" // 如果是 https 环境下
         });
         // 返回响应
         res.status(200).json({ message: "登录成功", status: 200, login_time });
