c67176dbca7e51a529ce57053ad43a4eb02b69db
durunsong <durunsong@sailvan.com>
2025-03-11 11:20:14 +0800

    💯fix:修改演示避免检测失败

--- routes/automation.js
+++ routes/automation.js
@@ -1,11 +1,18 @@
-
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'  // 手动指定正确的 Chrome 路径
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
   });
   const page = await browser.newPage();
-  await page.goto('https://www.dressin.com/');
+
+  // 规避检测
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
+  });
+
+  await page.goto('https://www.dressin.com/', { waitUntil: 'domcontentloaded' });
+
+  await browser.close();
 })();
